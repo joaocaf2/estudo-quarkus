9633ccaf0fe4a88646ec502e2fc07ff97228f605
João <Sem e-mail>
2025-03-22 16:36:02 -0300

    Criando Agencia Repository

--- src/main/java/br/com/alura/domain/Agencia.java
+++ src/main/java/br/com/alura/domain/Agencia.java
@@ -1,12 +1,24 @@
 package br.com.alura.domain;
 
 import br.com.alura.service.http.SituacaoCadastral;
+import jakarta.persistence.*;
 
+@Entity
 public class Agencia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+
+    @Column(name = "razao_social")
     private String razaoSocial;
+
     private String cnpj;
+
+    @OneToOne
+    @JoinColumn(name = "endereco_id")
+    private Endereco endereco;
+
     private SituacaoCadastral situacaoCadastral;
 
     public Integer getId() {
