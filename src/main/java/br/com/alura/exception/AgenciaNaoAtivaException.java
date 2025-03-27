dd26e1ea2694bc58e1ce4d27f5cfff0e657a6b5f
João <Sem e-mail>
2025-03-27 14:57:33 -0300

    Melhorando as exceptions personalizadas

--- src/main/java/br/com/alura/exception/AgenciaNaoAtivaException.java
+++ src/main/java/br/com/alura/exception/AgenciaNaoAtivaException.java
@@ -1,4 +1,8 @@
 package br.com.alura.exception;
 
 public class AgenciaNaoAtivaException extends RuntimeException {
+
+    public AgenciaNaoAtivaException(String message){
+        super(message);
+    }
 }
--- src/main/java/br/com/alura/exception/AgenciaNaoEncontradaException.java
+++ src/main/java/br/com/alura/exception/AgenciaNaoEncontradaException.java
@@ -1,4 +1,8 @@
 package br.com.alura.exception;
 
 public class AgenciaNaoEncontradaException extends RuntimeException {
+
+    public AgenciaNaoEncontradaException(String message) {
+        super(message);
+    }
 }
