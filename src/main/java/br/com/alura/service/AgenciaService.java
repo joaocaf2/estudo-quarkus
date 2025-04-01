2d1d7f374495481a77aa223adc4583f14217fae7
João <Sem e-mail>
2025-04-01 09:28:00 -0300

    Criando métricas

--- src/main/java/br/com/alura/service/AgenciaService.java
+++ src/main/java/br/com/alura/service/AgenciaService.java
@@ -5,6 +5,7 @@ import br.com.alura.exception.AgenciaNaoAtivaException;
 import br.com.alura.exception.AgenciaNaoEncontradaException;
 import br.com.alura.repository.AgenciaRepository;
 import br.com.alura.service.http.SituacaoCadastral;
+import io.micrometer.core.instrument.MeterRegistry;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
@@ -12,26 +13,32 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 public class AgenciaService {
 
     private final AgenciaRepository agenciaRepository;
+    private final MeterRegistry meterRegistry;
 
     @RestClient
     private SituacaoCadastralHttpService situacaoCadastralHttpService;
 
-    AgenciaService(AgenciaRepository agenciaRepository) {
+    AgenciaService(AgenciaRepository agenciaRepository, MeterRegistry meterRegistry) {
         this.agenciaRepository = agenciaRepository;
+        this.meterRegistry = meterRegistry;
     }
 
     public void cadastrar(Agencia agencia) {
         var agenciaHttpBuscada = situacaoCadastralHttpService.buscarPorCnpj(agencia.getCnpj());
 
         if (agenciaHttpBuscada == null) {
+            meterRegistry.counter("agencia_nao_adicionada_counter").increment();
             throw new AgenciaNaoEncontradaException("Agência não encontrada");
         }
 
         if (!agenciaHttpBuscada.getSituacaoCadastral().equals(SituacaoCadastral.ATIVO)) {
+            meterRegistry.counter("agencia_nao_adicionada_counter").increment();
             throw new AgenciaNaoAtivaException("Agência não ativa no momento");
         }
 
         agenciaRepository.persist(agencia);
+
+        meterRegistry.counter("agencia_adicionada_counter").increment();
     }
 
     public Agencia buscarPorId(Long id) {
